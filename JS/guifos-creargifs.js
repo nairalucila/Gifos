639f28ff261d79663a14a8465655ed4d2a6c4880
nairalucila <nairacoppari@gmail.com>
2020-04-07 12:30:34 -0300

    camara graba

--- JS/guifos-creargifs.js
+++ JS/guifos-creargifs.js
@@ -16,12 +16,15 @@ botonComenzar.addEventListener("click", function (e) {
   contenedorBoton.innerHTML = "";
   botonCaptura.style.visibility = "visible";
   contenedorCrearGif.style.height = "460px";
+
+  startCamera();
 });
 
 /////////////////// VIDEO /////////////////
-botonCaptura.addEventListener("click", function(ev){
-  
-  navigator.mediaDevices.getUserMedia({
+
+function startCamera(){
+
+navigator.mediaDevices.getUserMedia({
     audio: false,
     video: true,
     
@@ -29,17 +32,74 @@ botonCaptura.addEventListener("click", function(ev){
   .then(record)
   .catch (err => console.log(err)) 
  
-});
+}
 
   function record(stream){
     video.srcObject = stream;
-    let recorder = new GifRecorder(stream, {
-       type: gif,
-       frameRate: 500,
-       quality: 50,
-    })
+    
+    let mediaRecorder = new MediaRecorder(stream, {
+       mimeType: 'video/webm;codecs=h264'
+    });
 
-    }
+  mediaRecorder.start();
+
+  mediaRecorder.ondataavailable = function(e){
+ 
+    arr.push(e.data);
+
+  }
+
+  mediaRecorder.onstop = function(){
+    alert('fin grabacion');
+
+    let blob = new Blob(arr, {type:"video/webm"});
+    arr = [];
+    download(blob);
+    console.log('blobbb', blob);
+    console.log('arreglo', arr);
+
+  }
+
+  setTimeout(()=> mediaRecorder.stop(),5000)
+}
+ 
+///arreglo en donde se guardan los datos
+ let arr = [];
+
+ function download(blob){
+
+  let link =document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.setAttribute("download", "video_recorded.webm")
+  link.style.display = 'none';
+
+  document.body.appendChild(link);
+
+  link.click();
+  link.remove();
+  
+ }
+
+
+///////////// CAMBIO DE BOTON //////////////////////
+
+botonCaptura.addEventListener("click", function(ev){
+  ev.preventDefault();
+  
+  let divCamara = document.getElementById('divCamara');
+  let imagenDivCamara = document.getElementById('img-boton-captura')
+  let botonCamara = document.getElementById('cambtn');
+
+  divCamara.style.backgroundColor = '#FF6161' ;
+  botonCamara.style.backgroundColor = '#FF6161';
+  botonCamara.textContent = 'Listo'
+  imagenDivCamara.getAttribute('src', '/assets/recording.svg')
+
+
+
+ 
+  
+  });
 
 
 
