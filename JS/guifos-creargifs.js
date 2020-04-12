a4a4989f3be927e68181d3d0176e982715156267
nairalucila <nairacoppari@gmail.com>
2020-04-12 11:06:30 -0300

    avances nuevos

--- JS/guifos-creargifs.js
+++ JS/guifos-creargifs.js
@@ -1,5 +1,73 @@
-//CREACION DE ELEMENTOS
+///////////////// TEMAS  /////////////////////
 
+const LINK_DIA = "../CSS/vista-crear-g-light.css";
+let LINK_NOCHE = "../CSS/vista-crear-g-dark.css";
+const link_logo_dark = "/assets/gifOF_logo_dark.png";
+const link_logo_light = "/assets/gifOF_logo.png";
+let logoTag = document.getElementById("logoTag");
+
+let desplegable = document.getElementById("temas");
+let botonTema = document.getElementById("temabtn");
+
+//desplegar botonera
+const btnSDay = document.getElementById("sailorD");
+const btnSNight = document.getElementById("sailorN");
+
+const DIA_THEME_NAME = "DIAS";
+const NOCHE_THEME_NAME = "NOCHE";
+
+let hojaEstilo = document.getElementById("themeId");
+
+botonTema.addEventListener("click", function () {
+  desplegable.style.display = "flex";
+});
+
+let temaActual = DIA_THEME_NAME;
+
+btnSNight.addEventListener("click", function (ev) {
+  if (temaActual === DIA_THEME_NAME) {
+    hojaEstilo.setAttribute("href", LINK_NOCHE);
+    logoTag.setAttribute("src", link_logo_dark);
+    desplegable.style.display = "none";
+
+    temaActual = DIA_THEME_NAME;
+    localStorage.setItem("tema", hojaEstilo.setAttribute("href", LINK_NOCHE));
+  }
+});
+
+localStorage.getItem("tema");
+
+btnSDay.addEventListener("click", function (ev) {
+  // si tema == noche
+  if (temaActual === DIA_THEME_NAME) {
+    hojaEstilo.setAttribute("href", LINK_DIA);
+    logoTag.setAttribute("src", link_logo_light);
+    desplegable.style.display = "none";
+
+    temaActual = NOCHE_THEME_NAME;
+    localStorage.setItem("tema", hojaEstilo.setAttribute("href", LINK_NOCHE));
+    console.log("algo");
+  }
+});
+
+botonTema.addEventListener("focusout", function (ev) {
+  if (ev.relatedTarget === null) {
+    desplegable.style.display = "none";
+  } else {
+    return;
+  }
+});
+
+function extraerTemaNocheLocalStorage() {
+  if (temaActual === NOCHE_THEME_NAME) {
+    localStorage.getItem("tema", hojaEstilo.setAttribute("href", LINK_NOCHE));
+  } else {
+    localStorage.getItem("tema", hojaEstilo.setAttribute("href", LINK_DIA));
+  }
+}
+extraerTemaNocheLocalStorage();
+
+/////////////////// VIDEO /////////////////
 const instrucciones = document.getElementById("contenedorItems");
 const contenedor = document.getElementById("contenedorItems");
 const botonComenzar = document.getElementById("comenzar");
@@ -7,22 +75,19 @@ const video = document.getElementById("video");
 const contenedorBoton = document.getElementById("contenedorBotones");
 const botonCaptura = document.getElementById("boton_captura");
 const contenedorCrearGif = document.getElementById("contenedorCrearGif");
-let timerBar = document.getElementById("timerBar");
 
 botonComenzar.addEventListener("click", function (e) {
   e.preventDefault();
   instrucciones.remove();
   video.style.display = "block";
-  contenedorBoton.style.display = 'none';
+  contenedorBoton.style.display = "none";
   botonCaptura.style.visibility = "visible";
 
   startCamera();
 });
 
-/////////////////// VIDEO /////////////////
-
 let botonCamara = document.getElementById("cambtn");
-let botonListo  = document.getElementById('boton_listo')
+let botonListo = document.getElementById("boton_listo");
 
 let mediaRecorder; //variable del media recorder
 
@@ -30,11 +95,13 @@ function startCamera() {
   navigator.mediaDevices
     .getUserMedia({
       audio: false,
-      video: {width : 832 , height : 434},
+      video: { width: 832, height: 434 },
     })
-    .then(rec => record(rec))
+    .then((rec) => record(rec))
     .catch((err) => console.log(err));
 }
+let videoData = [];
+let isRecording = false;
 
 function record(stream) {
   video.srcObject = stream; //lo q tome de la camara lo pasa como string
@@ -44,59 +111,93 @@ function record(stream) {
     ev.preventDefault();
 
     getTimer();
-    timerBar.style.display = "flex";
-    // aca hay una var definida como falso. Cuando isRecoding es true, graba
+
+    // aca hay una var definida como falso. Cuando isRecoding es true, graba.
     isRecording = !isRecording;
-    if (isRecording === true) {
-      mediaRecorder = new RecordRTC(stream, {
-        type: "gif",
-        frameRate: 500,
-        width: 832,
-        height: 434,
-        onGifRecordingStarted: function () {
-          console.log("esta grabando");
-        },
-      });
-      mediaRecorder.startRecording();
-      mediaRecorder.camera = stream;
 
-      let divCamara = document.getElementById("divCamara");
-      let imagenDivCamara = document.getElementById("img-boton-captura");
+    mediaRecorder = new RecordRTC(stream, {
+      type: "gif",
+      frameRate: 500,
+      width: 832,
+      height: 434,
+      onGifRecordingStarted: function (e) {
+      console.log("esta grabando");
+      },
+    });
+    // mediaRecorder.ondataavailable = function(e) {
+    //   videoData.push(e.data);
+    // };
 
-      divCamara.style.backgroundColor = "#FF6161";
-      botonCamara.style.display = "none";
-      botonListo.style.display = 'block';
-      botonListo.style.position = 'relative';
-      botonListo.classList.add('boton_cam');
-      botonListo.style.backgroundColor = '#FF6161';
-      //imagenDivCamara.getAttribute(src, ".../assets/recording.svg");
-    } else {
-      mediaRecorder.stopRecording(stopRecordingCallback);
+    mediaRecorder.startRecording();
+    mediaRecorder.camera = stream;
 
-      stopedInterval = true;
-    }
+
+    botonCamara.style.display = "none";
+    botonListo.style.display = "block";
+    botonListo.style.position = "relative";
+    botonListo.classList.add("boton_cam");
+    botonListo.style.backgroundColor = "#FF6161";
   });
 }
 
-let isRecording = false;
+///////// Pones listo ///////////////////////////////
+let seccionCargaGif = document.getElementById("seccion_carga_gif");
+
+botonListo.addEventListener("click", function () {
+  timerBar.style.display = "flex";
+  
+  let barra = document.getElementById("barra");
+  arrancarBarraDeCarga(barra)
+  
+  console.log("listoo");
+
+  mediaRecorder.stopRecording(stopRecordingCallback);
+
+  stopedInterval = true;
+});
+
+/////////////// SECCION SUBIR Y REPETIR ////////////////////////
 
 let botonSubir = document.getElementById("boton_subir");
 let botonRepetir = document.getElementById("repetir");
 
-function stopRecordingCallback() {
-  mediaRecorder.camera.stop();
+function stopRecordingCallback(e) {
+ mediaRecorder.camera.stop();
+  // mediaRecorder.resumeRecording()
   quitarBotonListo();
-
-  //detiene la camara y se crea un formdata que es una funcion--
+  let blob = this.getBlob();
+  // let videoUrl = window.URL.createObjectURL(blob);
+  // console.log(mediaRecorder)
+  let videoEl = document.getElementById('video')
+  console.log(this.toURL())
+  console.log('LOOOOOOOOOOL', window.URL)
+  // video.srcObject =this.getBlob()
+  // video.play()
+  // videoEl.play()
+  // mediaRecorder.getDataURL(function(e) {
+  //   videoEl.src= e
+  // });
+  // videoEl.play();
+  //detiene la camara y se crea un formData
   let form = new FormData();
   form.append("file", mediaRecorder.getBlob(), "miGif.gif"); //a esta funcion se le pasan datos y el get blob contiene el gif
 
   console.log(form, "FORM");
 
+  //////
+
   botonSubir.addEventListener("click", function () {
-    
+    esconderBarraDeCarga();
+    seccionCargaGif.style.display = "block";
+
+
+    let barraDos = document.getElementById("barraSubiendo");
+    arrancarBarraDeCarga(barraDos);
     enviarGiphy(form) //cuando se hace click en boton subir - se ejcuta la funcion async que trae los datos del fetch con method post y body
-      .then((rep) => traerGifGuardarGaleria(rep.data.id)); //aca accedemos al id del gif y se lo pasamos a la fun que guarda ls
+      .then((rep) => {
+        traerGifGuardarGaleria(rep.data.id);
+        seccionCargaGif.style.display = "none";
+      }); //aca accedemos al id del gif y se lo pasamos a la fun que guarda ls
 
     // ocultar boton subir
   });
@@ -111,9 +212,10 @@ function quitarBotonListo() {
   botonCaptura.style.display = "none";
   contenedor.classList.add("contenedorCargaGif");
   contenedor.classList.remove("contenedorItems");
-
-  //aca deberia aparecer el boton de repetir captura
 }
+
+
+////////////////// FETCH /////////////////////////////
 
 const API_KEY = "DsV5wrnJyENgZWApbRea3zpRa7YSeHgd";
 const API_URL_UPLOAD = "http://upload.giphy.com/v1/gifs";
@@ -183,7 +285,9 @@ function iterarYAgregarEl() {
 
 iterarYAgregarEl();
 
-///// Timer
+//////////////////////// CONTADOR /////////////////
+
+let timerBar = document.getElementById("timerBar");
 
 let timer = document.getElementById("timer");
 let stopedInterval = false;
@@ -210,28 +314,24 @@ function getTimer() {
   }, 1000);
 }
 
-//// Barra de Carga
+///////// BARRA DE CARGA ///////////////////
 
-let barra = document.getElementById("barra");
 
-let count = 0;
-let restart = true;
-setInterval(() => {
-  if (restart) {
-    barra.children[count].style.backgroundColor = "pink";
+function arrancarBarraDeCarga(param) {
+  let count = 0;
+  setInterval(() => {
+    param.children[count].style.backgroundColor = "#F7C9F3";
     count++;
-  }
-  if (count == 17) {
-    for (let el of barra.children) {
-      el.style.backgroundColor = "blue";
+
+    if (count == param.children.length) {
+      for (let elemento of param.children) {
+        elemento.style.backgroundColor = "#999999";
+      }
+      count = 0;
     }
-    restart = true;
-    count = 0;
-  }
-}, 1000);
+  }, 1000);
+}
 
-
-///////////////// TEMAS  /////////////////////
-
-const TEMA_DIA = '..\CSS\vista-crear-g-light.css';
-const TEMA_NOCHE = '..\CSS\vista-crear-g-dark.css';
+function esconderBarraDeCarga() {
+  barra.style.display = "none";
+}
